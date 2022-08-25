94ae3738d11c0b5615793ff2ba1dc70633622117
Zachattack221 <zsvd09@gmail.com>
2022-08-25 17:49:06 -0400

    set up functions to save/pull information from local storage when page reloads

--- develop/script.js
+++ develop/script.js
@@ -2,3 +2,16 @@ var CurrentDayEl = moment().format('LL');
 console.log(CurrentDayEl);
 $("#currentDay").text(CurrentDayEl);
 
+$('.container').on('click', '.save', function () {
+    var noteTime = $(this).parent().attr('data-id');
+    var notesEl = $(this).prev().val();
+    localStorage.setItem(noteTime, notesEl);
+});
+
+$('.notes').each(function() {
+    var noteTime = $(this).parent().attr('data-id');
+    var notesEl = localStorage.getItem(noteTime);
+    $(this).text(notesEl);
+});
+
+//  write function that checks time agains moment() and changes the section colors accordingly
