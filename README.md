e757665143e802f16f7d92b50efc36f79f598db7
Zachattack221 <zsvd09@gmail.com>
2022-08-25 22:10:44 -0400

    updated README, finished function to assign color classes based on time in moment.js

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 2. [Visuals](#visuals)
 
 ## Description
+[Link to Live Site]()
+This project is a refactoring of a workday scheduler, one that allows a user to enter and save notes by the hour, in an interactive, color-coded format that scales with Moment.js timestamps. This repository utilizes jQuery & Bootstrap for formatting and functionality, taking advantage of local storage, and demonstrating event delegation in action.
 
 ## Visuals
+![Workday-scheduler](./assets/WorkdayScreenshot.png)
--- develop/script.js
+++ develop/script.js
@@ -7,28 +7,39 @@ $('.container').on('click', '.saveBtn', function () {
     localStorage.setItem(noteTime, notesEl);
 });
 
-$('.notes').each(function() {
+$('.notes').each(function () {
     var noteTime = $(this).parent().attr('data-id');
     var notesEl = localStorage.getItem(noteTime);
     $(this).text(notesEl);
 });
 
-//  write function that checks time agains moment() and changes the section colors accordingly
-// for (i = 9; i <= 17, i++) {
-    
+// created a function to check against a moment time, then assign new color classes based on that information
+function timeCurrent() {
     var timeCheck = moment().hour();
-console.log(timecheck);
+    $('.hour-row').each(function () {
+        var scheduleTime = parseInt($(this).attr('data-id'));
+        console.log(typeof(scheduleTime));
+        console.log(typeof(timeCheck));
+        if (scheduleTime < timeCheck) {
+            console.log('1');
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (scheduleTime === timeCheck) {
+            console.log('2');
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            console.log('3');
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+};
 
-// $('.container').each(function() {
-//     var timeCheck = moment().hour();
-//     for (var i = 9; i <= 17, i++) {
-//     if (i < timeCheck) {
-//         $(this).children.addclass(.past)
-//     }
-//     if (i > timecheck) {
-//         $(this).children.addclass(.future)
-//     }
-//     else {
-//         $(this).children.addclass(.present)
-//     }
-// }});
+
+timeCurrent();
