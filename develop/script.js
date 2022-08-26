9c73de73ffada2ebd4e8481e2ef2e1c9eb6f3a0b
Zachattack221 <zsvd09@gmail.com>
2022-08-25 20:11:19 -0400

    adjusted save icons, working on logic to assign classes to time rows compared to the hour in Moment.js

--- develop/script.js
+++ develop/script.js
@@ -1,8 +1,7 @@
 var CurrentDayEl = moment().format('LL');
-console.log(CurrentDayEl);
 $("#currentDay").text(CurrentDayEl);
 
-$('.container').on('click', '.save', function () {
+$('.container').on('click', '.saveBtn', function () {
     var noteTime = $(this).parent().attr('data-id');
     var notesEl = $(this).prev().val();
     localStorage.setItem(noteTime, notesEl);
@@ -15,3 +14,21 @@ $('.notes').each(function() {
 });
 
 //  write function that checks time agains moment() and changes the section colors accordingly
+// for (i = 9; i <= 17, i++) {
+    
+    var timeCheck = moment().hour();
+console.log(timecheck);
+
+// $('.container').each(function() {
+//     var timeCheck = moment().hour();
+//     for (var i = 9; i <= 17, i++) {
+//     if (i < timeCheck) {
+//         $(this).children.addclass(.past)
+//     }
+//     if (i > timecheck) {
+//         $(this).children.addclass(.future)
+//     }
+//     else {
+//         $(this).children.addclass(.present)
+//     }
+// }});
